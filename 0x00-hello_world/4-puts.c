ab81b13e649a30ea72f422a04b8afd1a8d223141
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-04-11 03:32:32 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilangual puzzle");
-  return (0);
+puts("\"Programming is like building a multilangual puzzle\n");
+return (0);
 }
