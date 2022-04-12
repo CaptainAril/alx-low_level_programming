7bf69a8254870f0cd9d645872cb4f61bcdeb2c29
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-04-11 21:28:38 -0700

    1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main - Entry point 
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet ,= 'z'; alphabet==)
+{
+putchar(alphabet);
+}
+return (0);
+}
