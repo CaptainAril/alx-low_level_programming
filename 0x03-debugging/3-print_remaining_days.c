b819b0ef3b6abd75704db0070cdb68f3ed89d4d9
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-04-14 13:06:33 -0700

    1

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,7 @@
  * Return: void
  */
 
-void print_remaining_day(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 {
 if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
@@ -23,9 +23,9 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day ++ 60)
+if (month == 2 && day == 60)
 {
-printf("Invalid date:: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
