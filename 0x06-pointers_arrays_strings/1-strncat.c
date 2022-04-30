c75140e7fa66cf158485828e9d6ecb8d8c529e1c
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-04-30 04:11:00 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: destination
+ * @src: string to be copied
+ * @n: amount of bytes from src used
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+  
+i = 0;
+j = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+while (j < n)
+{
+dest[i] = src[j];
+if (src[j] == '\0')
+break;
+i++;
+j++;
+}
+return (dest);
+}
