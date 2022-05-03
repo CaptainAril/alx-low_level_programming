8d22ca380ae4121bc3f01863afe3fcf30ca43d40
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-05-02 18:37:45 -0700

    Arguments to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the file name followed by a new line
+ * @argc: number of command line arguments
+ * @argv: array containing command line arguments
+ * Return: return 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
