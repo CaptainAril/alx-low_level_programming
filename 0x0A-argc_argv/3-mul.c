4248721878ccd7f86aae5184c157e4aecb39fdba
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-05-02 20:20:13 -0700

    Arguments to main

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,12 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-if (argc < 2)
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int mul = atoi(argv[1]) * atoi(argv[2]);
-printf("%d\n", mul);
+
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
