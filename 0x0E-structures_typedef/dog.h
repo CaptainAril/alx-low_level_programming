ea3946ef78f1257714de71527a1cfbd1fa920bf7
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-05-13 02:49:22 +0000

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,18 +16,8 @@ typedef struct dog
 char *name;
 float age;
 char *owner;
-}dog_t;
-
-/**
- * dog_t - typedef for struct dog
- * @name: name of dog
- * @age: age  of dog
- * @owner: owner of dog
- *
- */
-
-/* typedef struct dog dog_t; */
-
+}
+dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
