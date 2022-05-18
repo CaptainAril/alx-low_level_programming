b4a342ef7ac0e9a3ed391b20b282700e9b10f8eb
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-05-18 03:17:29 +0000

    function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,32 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: number of variadic parameters passed to the function
+ * @...: variable arguments
+ *
+ * Return: 0 - if n == 0
+ *             else, the sum of all the variable arguments
+ */
 
 int sum_them_all(const unsigned int n, ...)
 {
-  int sum;
-  unsigned int i; 
-  va_list sm;
-  sum = 0;
-  
-  if (n == 0)
-    {
-      return (0);
-    }
-  
-  va_start(sm, n);
-  for (i = 0; i < n; i++)
-    {
-      sum = va_arg(sm, int);
-    }
-  va_end(sm);
-  return(sum);
+int sum;
+unsigned int i;
+va_list sm;
+sum = 0;
+
+if (n == 0)
+{
+return (0);
+}
+
+va_start(sm, n);
+for (i = 0; i < n; i++)
+{
+sum = va_arg(sm, int);
+}
+va_end(sm);
+return(sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,21 +1,31 @@
+#include "varaidic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_numbers - a function that prints numbers, followed by a new line
+ * @seperator: the string to be printed betweent numbers
+ * @n: the number of integers passed to the function
+ * @...: variadic arguments of the function
+ *
+ * Return: print numbers passed into the variadic function, with seperator in between
+ *         if seperator == NULL, don't print
+ */
+
 void print_numbers(const char *seperator, const unsigned int n, ...)
 {
-  unsigned int i;
-  va_list pn;
+unsigned int i;
+va_list pn;
 
-  va_start(pn, n);
-  for (i = 0; i < n; i++)
-    {
-      printf("%d", va_arg(pn, int));
-      if (i != n - 1 && seperator != NULL)
-	{
-	  printf("%s", seperator);
-	}
-    }
-      va_end(pn);
-      printf("\n");
-     
+va_start(pn, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(pn, int));
+if (i != n - 1 && seperator != NULL)
+{
+printf("%s", seperator);
+}
+}
+va_end(pn);
+printf("\n");
 }
