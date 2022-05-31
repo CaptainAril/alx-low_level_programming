35ce23f538881f97aadc14110b33a1cb1ea50e23
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-05-31 20:32:06 +0000

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,42 +1,25 @@
 #include "main.h"
-#include <ctype.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
 
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, len;
-	unsigned int num, x, y;
+	int i;
+	unsigned int dec_val = 0;
 
-	i = 0;
-	num = 0;
-	x = 0;
-	y = 0;
-	len = 0;
+	if (!b)
+		return (0);
 
-	if (b == NULL)
-		return 0;
-	len = strlen(b);
-
-	while (b[i])
+	for (i = 0; b[i]; i++)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return 0;
-		else
-		{
-			x = ((unsigned int)(b[i]) - '0');
-			printf("%d\t", x);
-/*			printf("len: %d\t", len);
-			y = x * 2;
-			printf("%d\t", y);
-			num += y;
-			printf("%d\n", num);*/
-			i++;
-		}
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	i = len+y;
-	printf("\t---------\n");
-	return num;
+
+	return (dec_val);
 }
