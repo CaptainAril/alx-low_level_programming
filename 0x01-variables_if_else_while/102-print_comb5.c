ab67320ff0d2dc702420245733a59534fed47f31
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-06-24 18:03:00 +0000

    .

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,19 +10,23 @@ int main(void)
 {
 	int a, b, c, d;
 
-	for (a = 48;  a < 57; a++)
-		for (b = 48; b < 57; b++)
-			for (c = 48; b < 57; c++)
-				for (d = 48; d < 57; d++)
-				{
-					putchar(a);
-					putchar(b);
-					putchar(32);
-					putchar(c);
-					putchar(d);
-					putchar(44);
-					putchar(32);
-				}
+	for (a = 48;  a < 58; a++)
+		for (b = 48; b < 58; b++)
+			for (c = 48; c < 58; c++)
+				for (d = 48; d < 58; d++)
+					if (a <= c && b < d)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(32);
+						putchar(c);
+						putchar(d);
+						if (!(a == 57 && b == 56 && c == 57 && d == 57))
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
 	putchar('\n');
 
 	return (0);
