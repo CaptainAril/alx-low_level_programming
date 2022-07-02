2ee1c4c6f8dc349468ee7f38a296c5c2631d4d24
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-02 19:32:46 +0000

    betty fixes

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	long int a = 0, b = 1, fib_num, i;
+	long int a = 1, b = 1, fib_num, i;
 
-	for (i = 0; i <= 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		fib_num = a;
 		printf("%li", fib_num);
 		a += b;
 		b = fib_num;
-		if (i < 50)
+		if (i < 49)
 			printf(", ");
 	}
 	putchar('\n');
