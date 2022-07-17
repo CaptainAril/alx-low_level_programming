ebb46c3bee24a4b419f646e9eb00b9d0226439d7
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-17 03:44:24 +0000

    dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/CaptainAril/alx-low_level_programming/master/0x18-dynamic_libraries/libwin.so
+wget -P /tmp/ https://github.com/CaptainAril/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libwin.so
 export LD_PRELOAD=/tmp/libwin.so
