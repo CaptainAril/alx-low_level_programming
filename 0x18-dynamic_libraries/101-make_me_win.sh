c8c61d62caa3ae5f3fb20535e59e8fa32a1c4e11
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-17 04:11:58 +0000

    1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/CaptainAril/alx-low_level_programming/blob/ecbdaa19eda93a2769923ef46aec1916be0763f2/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/CaptainAril/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
