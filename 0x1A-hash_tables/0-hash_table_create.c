5dc6f8bebc00a8139bebb92c59cc674202450415
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-29 03:19:24 +0000

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = malloc(sizeof(hash_table_t));
+	ht = malloc(sizeof(hash_table_t) * size);
 	if (ht == NULL)
 	{
 		free(ht);
 		return (NULL);
 	}
 	ht->size = size;
-	ht->array = calloc((ht->size), sizeof(hash_node_t));
+/*	ht->array = calloc((ht->size), sizeof(hash_node_t)); */
 	return (ht);
 }
