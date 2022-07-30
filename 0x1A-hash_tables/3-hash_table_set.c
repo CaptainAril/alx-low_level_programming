4c23ae0d98a844661cab1a30055b1adf4aebb808
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-30 04:23:38 +0000

    hash tables

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table we want to add or update key/value to
+ * @key: the key. @Key cannot be an empty sting
+ * @value: the value associated with the key.
+ * Value must be duplicated, and can be an empty string
+ *
+ * Return: 1 if successful; 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	return 0;
+}
