dd1707fb6d50eb0c0e3c6cb2b93c9416566e172a
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2022-07-31 17:20:58 +0000

    hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,10 +13,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *) key, ht->size);
 
 	node = ht->array[index];
-	if (node == NULL)
-		return (NULL);
-	return (node->value);
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
 }
