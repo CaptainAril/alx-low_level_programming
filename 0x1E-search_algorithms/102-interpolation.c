ce0ff9eadf4beca5f3885a94bcda6d8ac0f79b31
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-07 14:55:00 +0100

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -19,7 +19,7 @@ int interpolation_search(int *array, size_t size, int value)
 	low = 0;
 	high = size - 1;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while ((array[high] != array[low]) && (value >= array[low]) &&
