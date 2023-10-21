b4d6e43544dd55b65bb61ae41b109c77153dec30
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-10-21 23:06:23 +0100

    refactored code

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,20 @@
  */
 char *rot13(char *str)
 {
-	int i;
+	int i, j;
+	char *s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *t = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; str[i]; i++)
 	{
-		if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
-			str[i] += 13;
-		else if ((str[i] >= 'N' && str[i] <= 'Z') || (str[i] >= 'n' && str[i] <= 'z'))
-			str[i] -= 13;
+		for (j = 0; s[j]; j++)
+		{
+			if (str[i] == s[j])
+			{
+				str[i] = t[j];
+				break;
+			}
+		}
 	}
 	return (str);
 }
