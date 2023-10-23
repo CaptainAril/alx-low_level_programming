96366b4c9ffd2ccff9f902cc122683e76d39999c
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-10-23 11:33:24 +0100

    refactored code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,15 @@
  */
 void print_number(int n)
 {
-	unsigned int x;
 
-	x = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		x *= -1;
+		n *= -1;
 	}
 
-	if ((x / 10) > 0)
-		print_number(x / 10);
+	if ((n / 10) > 0)
+		print_number(n / 10);
 
-	_putchar((x % 10) + '0');
+	_putchar((n % 10) + '0');
 }
